3b2bba9bbddcaf1d791836dd16be199c044270dd
Dipanshu Kumar <dipanshuuk711@gmail.com>
2024-06-05 09:51:17 +0530

    Changes to the backend

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
 import bodyParser from "body-parser";
+import userRouter from "./routes/userRoute.js";
+import 'dotenv/config'
 
 const app = express();
 const port = 3000
@@ -17,12 +19,12 @@ app.use(cors());
 //API endpoint      
 app.use('/api/food', foodRouter);
 app.use('/images',express.static('uploads'));
+app.use("/api/user", userRouter)
 
 
 app.get('/', (req,res)=>{
      res.send('API working')
 });
-
 app.listen(port, ()=>{
      console.log(`Server is running on http://localhost:${port}`)
 })    
